dd566ec9c1027cbc7bfb1ac32f817d367b514e5a
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-05-30 23:25:31 +0700

    i9082: advertise bluetooth LE capabilities
    Change-Id: I041838db691948896c3b5493f09e2dbd0089fe9a

--- device_i9082.mk
+++ device_i9082.mk
@@ -58,6 +58,7 @@ PRODUCT_PACKAGES += \
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
 	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
