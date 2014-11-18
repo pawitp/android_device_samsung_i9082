40478b1406a8da81e306ac040d12cdd78bb6aa31
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-11-18 10:20:45 +0700

    Use Google's centralized media_codecs parts
    Fixes .wav playback
    
    Change-Id: I92a25e2bcd3da7c15fe467651f5e9f1da502e917

--- device_i9082.mk
+++ device_i9082.mk
@@ -21,6 +21,9 @@ PRODUCT_COPY_FILES += \
 	device/samsung/i9082/fstab.capri_ss_baffin:root/fstab.capri_ss_baffin \
 
 PRODUCT_COPY_FILES += \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 	device/samsung/i9082/media_codecs.xml:system/etc/media_codecs.xml \
 
 # Prebuilt kl keymaps
