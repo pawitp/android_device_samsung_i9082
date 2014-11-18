192e8f1b74dc0f07b76844e96ec5d7361c95ffc4
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-11-18 22:48:45 +0700

    i9082: use android standardized charger mode instead of lpm.rc
    See http://review.cyanogenmod.org/#/c/77926/
    
    Change-Id: If29e231566f0de16d4bf825df8ee53f719c4f729

--- device_i9082.mk
+++ device_i9082.mk
@@ -16,7 +16,6 @@ PRODUCT_COPY_FILES += \
 	device/samsung/i9082/init.bcm281x5.usb.rc:root/init.bcm281x5.usb.rc \
 	device/samsung/i9082/init.log.rc:root/init.log.rc \
 	device/samsung/i9082/init.recovery.capri_ss_baffin.rc:root/init.recovery.capri_ss_baffin.rc \
-	device/samsung/i9082/lpm.rc:root/lpm.rc \
 	device/samsung/i9082/ueventd.capri_ss_baffin.rc:root/ueventd.capri_ss_baffin.rc \
 	device/samsung/i9082/fstab.capri_ss_baffin:root/fstab.capri_ss_baffin \
 
@@ -57,7 +56,6 @@ PRODUCT_PACKAGES += \
 
 # Charger
 PRODUCT_PACKAGES += \
-	charger \
 	charger_res_images
 
 # These are the hardware-specific features
