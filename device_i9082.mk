2153bf8eadc09390f7175d36f029f1483edb360c
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-11-27 18:34:29 +0700

    i9082: add FFMPEG codecs
    Change-Id: Ia51ef8fa365aad21623df6fd659ef64860225d1b

--- device_i9082.mk
+++ device_i9082.mk
@@ -23,6 +23,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
 	device/samsung/i9082/media_codecs.xml:system/etc/media_codecs.xml \
 
 # Prebuilt kl keymaps
