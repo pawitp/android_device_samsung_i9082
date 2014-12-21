07b0e49f9c3f433b48e0612757a56ff50bcb2d05
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-12-21 15:58:59 +0700

    i9082: enable miracast
    Need to move audio_policy.conf here to fix remote submix configuration
    
    Change-Id: Icbc200ac93af614531145b33cf7af3003e50f39a

--- device_i9082.mk
+++ device_i9082.mk
@@ -22,6 +22,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
 	device/samsung/i9082/media_codecs.xml:system/etc/media_codecs.xml \
+	device/samsung/i9082/audio_policy.conf:system/etc/audio_policy.conf \
 
 # Prebuilt kl keymaps
 PRODUCT_COPY_FILES += \
