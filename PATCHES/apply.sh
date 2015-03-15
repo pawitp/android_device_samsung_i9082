b11279ba883991ca94206163f702f5ab2616b0d4
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-03-15 12:55:31 +0700

    i9082: update patches
    - Add script for applying patch
    - Fix wifi tethering
    - Fix crash when dialing from Contacts app
    - Fix bluetooth
    - Fix GPS status reporting
    - Remove ellipsis for dialer (AOSP 5.1 supports 2 line without clipping)
    
    Change-Id: If323bb3c86a0d492040effcf92d16c89d61f4b79

---
+++ PATCHES/apply.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in *.patch
+do
+	folder=`echo $i | sed 's/_/\//g' | sed 's/\.patch//g' | sed 's/\/legacy/_legacy/g' `
+	current=`pwd`
+	echo $folder
+	pushd ../../../../$folder
+
+	git reset --hard refs/tags/android-5.1.0_r1
+	git am < $current/$i
+
+	popd
+done
