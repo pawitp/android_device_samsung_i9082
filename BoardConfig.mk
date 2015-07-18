d86fb87fa631b34b8723c775c761fe2e6253c96c
Pawit Pornkitprasan <p.pawit@gmail.com>
2015-07-18 03:21:51 -0400

    i9082: set default recovery density to hdpi
    This allows the recovery of the correct density to be
    built without hacking build/
    
    Change-Id: Icb233bad7090636e86bf8ea6b0c6db893d4e7364

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,6 +94,7 @@ BOARD_RIL_CLASS := ../../../device/samsung/i9082/ril/
 TARGET_RECOVERY_FSTAB := device/samsung/i9082/fstab.capri_ss_baffin
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
 TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
+TARGET_RECOVERY_DENSITY := hdpi
 
 # healthd
 BOARD_HAL_STATIC_LIBRARIES := libhealthd.capri
